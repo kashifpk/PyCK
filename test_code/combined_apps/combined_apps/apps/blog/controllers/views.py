930f260ab5928186f99da63db520638615ad299f
Kashif Iftikhar <kashif@compulife.com.pk>
2011-12-30 00:13:35 +0500

    Further improvements in sample app. Application templates have access to three global variables PROJECT_NAME, APP_NAME, APP_BASE now for further easing the development of relocatable templates.

--- test_code/combined_apps/combined_apps/apps/blog/controllers/views.py
+++ test_code/combined_apps/combined_apps/apps/blog/controllers/views.py
@@ -7,10 +7,9 @@ from ..models.models import (
 
 from .. import APP_NAME, PROJECT_NAME, APP_BASE
 
-#@view_config(route_name='blog.home', renderer='combined_apps:apps/blog/templates/mytemplate.mako')
 @view_config(route_name=APP_NAME+'.home', renderer='%s:templates/mytemplate.mako' % APP_BASE)
 def my_view(request):
     #one = DBSession.query(MyModel).filter(MyModel.name=='one').first()
     one=None
     
-    return {'APP_BASE': APP_BASE}
+    return {}
