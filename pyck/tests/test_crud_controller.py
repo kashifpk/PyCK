b538407fc1d6d0a156f0b6931a8917f6e3e723a5
Kashif Iftikhar <kashif@compulife.com.pk>
2014-11-07 23:21:00 +0500

    Unit tests for CRUDController

--- pyck/tests/test_crud_controller.py
+++ pyck/tests/test_crud_controller.py
@@ -10,17 +10,27 @@ class UserCRUDController(CRUDController):
     db_session = db_session
 
 
-#class TestCRUD(unittest.TestCase):
-#
-#    def setUp(self):
-#        self.config = testing.setUp()
-#        self.config.include('pyramid_handlers')
-#        Base.metadata.create_all(db_engine)
-#        add_crud_handler(self.config, 'user', '/user', UserCRUDController)
-#
-#    def tearDown(self):
-#        testing.tearDown()
-#
-#    def test_01_crud_controller(self):
-#
-#        pass
+class TestCRUD(unittest.TestCase):
+
+    def setUp(self):
+        self.config = testing.setUp()
+        self.config.include('pyramid_handlers')
+        Base.metadata.create_all(db_engine)
+        add_crud_handler(self.config, 'user', '/user', UserCRUDController)
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_01_crud_controller_routes(self):
+        "Test to check all CRUD routes are created correctly"
+
+        introspector = self.config.introspector
+        crud_routes = {'userCRUD_list': '/user/',
+                       'userCRUD_add': '/user/add',
+                       'userCRUD_edit': '/user/edit/{PK}',
+                       'userCRUD_details': '/user/details/{PK}',
+                       'userCRUD_delete': '/user/delete/{PK}'}
+
+        for route_name in crud_routes:
+            self.assertIsNotNone(introspector.get('routes', route_name))
+            self.assertEquals(introspector.get('routes', route_name)['pattern'], crud_routes[route_name])
